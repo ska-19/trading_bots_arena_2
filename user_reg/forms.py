07cffacaf3ed064d51bc0b196e098d11023b0f9d
ska-19 <ska04@icloud.com>
2023-06-05 11:46:10 +0300

    user_profile valid and icons

--- user_reg/forms.py
+++ user_reg/forms.py
@@ -18,3 +18,12 @@ class RegisterUserForm(UserCreationForm):
 class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+
+class UpdateProfileForm(forms.ModelForm):
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    class Meta:
+        model = User
+        fields = ['username']
--- user_reg/urls.py
+++ user_reg/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('reg/', RegisterUser.as_view(), name='signup'),
     path('login/',LoginUser.as_view(),name='login'),
     path('logout/', logout_user, name='logout'),
-    # path('signup/', views.user_signup, name='signup'),
     path('profile/<int:id>', profile, name='profile'),
     path('userprofile/<int:id>', user_profile, name='userprofile'),
+
 
 ]
