5aa5d09cc02a14c34ec9f7b80577cc87c468c557
ska-19 <ska04@icloud.com>
2023-06-06 12:13:53 +0300

    bot_detail + text-center

--- bot_ui/urls.py
+++ bot_ui/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     path('', RegisterBot.as_view(), name='add_bot'),
     path('add/', RegisterBot.as_view(), name='add_bot'),
-    # path('list/', bots_list, name='bots_list'),
     path('list/', BotListView.as_view(), name='bots_list'),
+    path('detail/<int:pk>', bot_detail_view, name='bot_detail'),
+
 ]
