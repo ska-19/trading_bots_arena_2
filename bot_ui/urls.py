ea8993dffbba19e1733a7bcd3bc28b5a42bb0b1f
Sensorzxc <sensor.main@gmail.com>
2023-06-08 02:30:49 +0300

    add custom permission + edit_bot + delete_bot

--- bot_ui/urls.py
+++ bot_ui/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('', RegisterBot.as_view(), name='add_bot'),
     path('add/', RegisterBot.as_view(), name='add_bot'),
     path('list/', BotListView.as_view(), name='bots_list'),
-    path('detail/<int:pk>', bot_detail_view, name='bot_detail'),
+    path('detail/<int:pk>', BotDetailView.as_view(), name='bot_detail'),
     path('load_Change_Balance/<int:pk>', load_change_balance, name='load_change_balance'),
-
+    path('delete/<int:pk>', BotDeleteView.as_view(), name='bot_delete'),
+    path('ranking/', AllBotListView.as_view(), name='ranking'),
+    path('update/<int:pk>', BotUpdateView.as_view(), name='bot_edit'),
 ]
